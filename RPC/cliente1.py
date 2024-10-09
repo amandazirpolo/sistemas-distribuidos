876db51edf2f063579dc2cfb9f604de3fa6ac13c
amandazirpolo <amandaszirpolo@gmail.com>
2024-10-09 16:54:22 -0300

    trabalho 1

---
+++ RPC/cliente1.py
@@ -0,0 +1,16 @@
+import rpyc
+import sys
+import time
+
+#start = time.time()
+if len(sys.argv) < 2:
+    exit("Usage {} SERVER".format(sys.argv[0]))
+
+server = sys.argv[1]
+conn = rpyc.connect(server,18861)
+
+print(c.root)
+print(c.root.get_answer())
+print(c.root.the_real_answer_though)
+#end = time.time()
+#print(end-start)
---
+++ RPC/cliente2.py
@@ -0,0 +1,15 @@
+import rpyc
+import sys
+import os
+import time
+
+#start = time.time()
+if len(sys.argv) < 2:
+    exit("Usage {} SERVER".format(sys.argv[0]))
+
+server = sys.argv[1]
+conn = rpyc.connect(server,18861)
+
+print(c.get_question)
+#end = time.time()
+#print(end-start)
